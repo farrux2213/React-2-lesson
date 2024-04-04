02641206bb571e125141afd0424f4d0db9d8f9c1
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-04-04 22:38:48 +0500

    no commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="head">
+      <div className="headLeft"></div>
+      <div className="headRight"></div>
+    </div>
+    <div></div>
+  );
+}
+
+export default App;

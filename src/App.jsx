ce4946c55a4e733d22204ffa81969a06b9806cf2
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-04-06 00:44:33 +0500

    ;lbs

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,51 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="head">
-      <div className="headLeft"></div>
-      <div className="headRight"></div>
-    </div>
-    <div></div>
+    <>
+      <div className="head">
+        <div className="headLeft"></div>
+        <div className="headRight"></div>
+      </div>
+      <div className="followers">
+        <div className="followers-top">
+          <h1>Check This Before Hiring Anyone</h1>
+        </div>
+        <div className="followers-bottom">
+          <div className="K-plus-1">
+            <h1>150K+</h1>
+            <p>Freelance Worker</p>
+          </div>
+          <div className="K-plus-2">
+            <h1>7K+</h1>
+            <p>Professional Clients</p>
+          </div>
+          <div className="K-plus-3">
+            <h1>8K+</h1>
+            <p>Engineering Expart</p>
+          </div>
+          <div className="K-plus-4">
+            <h1>2K+</h1>
+            <p>Top Companies</p>
+          </div>
+        </div>
+      </div>
+      <div className="six-box">
+        <div className="all-six-box">
+          <div className="first-series">
+            <div className="first-series-1"></div>
+            <div className="first-series-1"></div>
+          </div>
+          <div className="second-series">
+            <div className="second-series-1"></div>
+            <div className="second-series-2"></div>
+          </div>
+          <div className="third-series">
+            <div className="third-series-1"></div>
+            <div className="third-series-2"></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 

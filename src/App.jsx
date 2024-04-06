2b446d4dc68761c2970cef25ef8f5215655d4939
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-04-06 12:18:57 +0500

    end six box area

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import "./App.css";
+import insunicon from "/insunicon.svg";
+import sixline from "/sixline.svg";
+import staricon from "/staricon.svg";
+import unvonicon from "/unvonicon.svg";
+import searchicon from "/searchicon.svg";
+import bagicon from "/bagicon.svg";
 
 function App() {
   return (
@@ -33,16 +39,58 @@ function App() {
       <div className="six-box">
         <div className="all-six-box">
           <div className="first-series">
-            <div className="first-series-1"></div>
-            <div className="first-series-1"></div>
+            <div className="first-series-1">
+              <h1>Our Features To Find Talent</h1>
+              <button>
+                <p className="button-p">Try For Free</p>
+              </button>
+            </div>
+            <div className="first-series-2">
+              <div className="sunicon">
+                <img src={insunicon} className="insunicon" />
+              </div>
+              <h2>Strong Social Proof</h2>
+              <p>Impress your boss with a professional in UI design.</p>
+            </div>
+            <img src={sixline} className="sixline" />
           </div>
           <div className="second-series">
-            <div className="second-series-1"></div>
-            <div className="second-series-2"></div>
+            <div className="second-series-1">
+              <div className="sunicon">
+                <img src={staricon} className="staricon" />
+              </div>
+              <h2>Collected Review</h2>
+              <p>Impress your boss with a professional in UI design.</p>
+            </div>
+            <img src={sixline} className="sixline2" />
+
+            <div className="second-series-2">
+              <div className="sunicon">
+                <img src={unvonicon} className="unvonicon" />
+              </div>
+              <h2>Top Rated Telent</h2>
+              <p>Impress your boss with a professional in UI design.</p>
+            </div>
+            <img src={sixline} className="sixline3" />
           </div>
           <div className="third-series">
-            <div className="third-series-1"></div>
-            <div className="third-series-2"></div>
+            <div className="third-series-1">
+              <div className="sunicon">
+                <img src={searchicon} className="searchicon" />
+              </div>
+              <h2>Find Out Project</h2>
+              <p>Impress your boss with a professional in UI design.</p>
+            </div>
+            <img src={sixline} className="sixline4" />
+
+            <div className="third-series-2">
+              <div className="sunicon">
+                <img src={bagicon} className="bagicon" />
+              </div>
+              <h2>Portfolio Creator</h2>
+              <p>Impress your boss with a professional in UI design.</p>
+            </div>
+            <img src={sixline} className="sixline5" />
           </div>
         </div>
       </div>

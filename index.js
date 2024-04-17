0ad3e945d14ebd7d993e719990d05c908b5bbf0d
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-04-17 22:16:34 +0500

    hard problem

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-function sumAndValues(nums, andValues) {
-  let sum = 0;
-  for (let i = 0; i < andValues.length; i++) {
-    let currentSum = 0;
-    let start = andValues[i];
-    while (start < nums.length && nums[start] === andValues[i]) {
-      currentSum += nums[start];
-      start++;
+function fullJustify(words, maxWidth) {
+  const result = [];
+  let line = [];
+  let lineLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (lineLength + line.length + word.length > maxWidth) {
+      result.push(justifyLine(line, maxWidth, false));
+      line = [];
+      lineLength = 0;
     }
-    sum += currentSum;
+    line.push(word);
+    lineLength += word.length;
   }
-  return sum;
+
+  // Justify the last line (left-justified)
+  result.push(justifyLine(line, maxWidth, true));
+
+  return result;
+}
+
+function justifyLine(words, maxWidth, isLastLine) {
+  const spacesNeeded =
+    maxWidth - words.reduce((acc, word) => acc + word.length, 0);
+  const spacesBetweenWords = words.length - 1;
+
+  if (isLastLine || spacesBetweenWords === 0) {
+    // Left-justify the last line or if only one word in the line
+    return (
+      words.join(" ") +
+      " ".repeat(maxWidth - words.join("").length - spacesBetweenWords)
+    );
+  }
+
+  const spacesPerWord = Math.floor(spacesNeeded / spacesBetweenWords);
+  const extraSpaces = spacesNeeded % spacesBetweenWords;
+
+  let justifiedLine = "";
+  for (let i = 0; i < words.length - 1; i++) {
+    justifiedLine +=
+      words[i] + " ".repeat(spacesPerWord + (i < extraSpaces ? 1 : 0));
+  }
+  justifiedLine += words[words.length - 1]; // Last word without extra spaces
+
+  return justifiedLine;
 }
 
 // Test case
-const nums = [1, 4, 3, 3, 2];
-const andValues = [0, 3, 3, 2];
-console.log(sumAndValues(nums, andValues)); // Output: 12
+const words = ["This", "is", "an", "example", "of", "text", "justification."];
+const maxWidth = 16;
+console.log(fullJustify(words, maxWidth));
